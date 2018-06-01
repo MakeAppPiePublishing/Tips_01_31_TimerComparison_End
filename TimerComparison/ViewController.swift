5938624d56fc4da5b65cb14adcd7d35dd7b59ee9
Steve Lipton <stevelipton@brk-16892.local>
2018-06-01 15:28:07 -0500

    Initial Commit End state

--- TimerComparison/ViewController.swift
+++ TimerComparison/ViewController.swift
@@ -10,6 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var differenceLabel: UILabel!
+    let interval = 1.0/30.0
+    var timer = Timer()
+   
+    @IBAction func didToggleTimer(_ sender: UIButton) {
+       var timerCount = 0.0
+        let startDate = Date()
+        if !timer.isValid{
+            timer = Timer.scheduledTimer(withTimeInterval: interval, repeats: true, block:
+                { (timer) in
+                    timerCount += self.interval
+                    self.countLabel.text = String(format:"Timer %06.4f",timerCount)
+                    let duration = DateInterval(start: startDate, end: Date()).duration
+                    self.dateLabel.text = String(format:"Date %06.4f",duration)
+                    self.differenceLabel.text = String(format:"Diffrence %06.4f",duration - timerCount)
+            })
+        } else {
+            timer.invalidate()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
